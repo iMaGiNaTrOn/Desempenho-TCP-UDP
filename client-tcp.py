e8019672ea87a4036683f782d968ad8953718ed4
Gabriel Norato <43974551+iMaGiNaTrOn@users.noreply.github.com>
2023-07-16 13:16:36 -0300

    Add files via upload

---
+++ client-tcp.py
@@ -0,0 +1,78 @@
+import socket
+import os
+
+def send_file(client_socket, filename, packet_size):
+    print(filename)
+    file = open(filename, 'rb')
+    i = 0
+    while True:
+        data = file.read(packet_size)
+        if not data: 
+            break
+        client_socket.send(data)
+        # print("pacote", i)
+        i += 1
+    print("Enviando pacote vazio")
+    client_socket.send(b'')
+    print('Arquivo', filename, 'enviado com sucesso.')
+    file.close()
+
+def main():
+    # Configurar o endereço do servidor
+    host = '192.168.100.111'  # Endereço IP do servidor
+    port = 12345  # Porta usada pelo servidor
+
+    # Cria um objeto socket TCP/IP
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # Conecta ao servidor remoto
+    client_socket.connect((host, port))
+
+    while True:
+        # Obtém uma mensagem do usuário
+        message = input('Digite uma mensagem para enviar ao servidor (ou "enviar" para enviar um arquivo, e "sair" para encerrar): ')
+        if message == 'sair':
+            client_socket.shutdown(socket.SHUT_RDWR)
+            break
+        elif message == 'enviar':
+            # Envia a mensagem para o servidor
+            client_socket.send(message.encode())
+            
+            # Obtém o nome do arquivo a ser enviado
+            filename = input('Digite o nome do arquivo a ser enviado: ')
+            
+            # Envia o nome do arquivo para o servidor
+            client_socket.send(filename.encode())
+            
+            # Seleciona o tamanho do pacote
+            packet_size = int(input('Digite o tamanho do pacote (100, 500 ou 1000 bytes): '))
+            
+            # Envia o tamanho do pacote
+            client_socket.send(str(packet_size).encode())
+            
+            # Obtém o tamanho do arquivo
+            file_size = os.path.getsize(filename)
+            print(file_size, end="\n\n")
+            # Envia o tamanho do arquivo para o servidor
+            client_socket.send(str(file_size).encode())
+            
+            teste = client_socket.recv(1024).decode()
+            print(teste)
+            
+            # Chama a função para enviar o arquivo
+            send_file(client_socket, filename, packet_size)
+        
+        else:
+            # Envia a mensagem ao servidor
+            client_socket.send(message.encode())
+
+        # Recebe a resposta do servidor
+        response = client_socket.recv(1024).decode()
+
+        print('Resposta do servidor:', response)
+
+    # Fecha a conexão com o servidor
+    client_socket.close()
+
+if __name__ == '__main__':
+    main()
---
+++ client-udp.py
@@ -0,0 +1,65 @@
+import socket
+import os
+
+def send_file(client_socket, filename, packet_size, host, port):
+    with open(filename, 'rb') as file:
+        while True:
+            data = file.read(packet_size)
+            if not data:
+                break
+            client_socket.sendto(data, (host, port))
+            
+            #Esperando a confirmação de recebimento do server
+            try:
+                client_socket.settimeout(1) # Define um timeout de 1 segundo para receber
+                confirmacao, _ = client_socket.recvfrom(1024)
+                client_socket.settimeout(None)
+            except socket.timeout:
+                print('Timeout: Pacote não recebido, retransmitindo...')
+                continue
+            
+        # Envia uma mensagem vazia para indicar o final do arquivo
+        client_socket.sendto(b'', (host, port))
+    print('Arquivo', filename, 'enviado com sucesso.')
+
+def main():
+    host = '192.168.100.111'  # Endereço IP do servidor
+    port = 12345  # Porta usada pelo servidor
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    while True:
+        message = input('Digite uma mensagem para enviar ao servidor (ou "enviar" para enviar um arquivo, "sair" para encerrar): ')
+        if message == 'sair':
+            client_socket.shutdown(socket.SHUT_RDWR)
+            break
+        elif message == 'enviar':
+            # Envia a mensagem para o servidor
+            client_socket.sendto(message.encode(), (host, port))
+            # Obtém o nome do arquivo a ser enviado
+            filename = input('Digite o nome do arquivo a ser enviado: ')
+            # Envia o nome do arquivo para o servidor
+            client_socket.sendto(filename.encode(), (host, port))
+            # Obtém o tamannho do pacote a ser enviado
+            packet_size = int(input('Digite o tamanho do pacote (100, 500 ou 1000 bytes): '))
+            # Envia o tamanho do pacote para o servidor
+            client_socket.sendto(str(packet_size).encode(), (host, port))
+            
+            # Obtém o tamanho do arquivo
+            file_size = os.path.getsize(filename)
+            print(file_size, end="\n\n")
+            # Envia o tamanho do arquivo para o servidor
+            client_socket.sendto(str(file_size).encode(), (host, port))
+            
+            # Chama a função para enviar o arquivo
+            send_file(client_socket, filename, packet_size, host, port)
+            
+            # Recebe confirmação de término de recebimento
+            
+        else:
+            client_socket.sendto(message.encode(), (host, port))
+
+    client_socket.close()
+
+if __name__ == '__main__':
+    main()
